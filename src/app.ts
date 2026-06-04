c84997738cff87335d9f8d54c699a58fa6e16275
erickbento10 <erickbento@yahoo.com.br>
2026-06-04 18:37:55 -0300

    ajuste nas paginas

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,24 @@
-function iniciarSistema(): void {
-    console.log("SalonTech iniciado com sucesso!");
+const btn = document.getElementById("toggleTheme") as HTMLButtonElement;
+
+btn.addEventListener("click", () => {
+    document.body.classList.toggle("dark");
+});
+
+// Data atual
+function mostrarData(): void {
+    const hoje = new Date();
+
+    const data = hoje.toLocaleDateString("pt-BR", {
+        day: "numeric",
+        month: "long",
+        year: "numeric"
+    });
+
+    const el = document.getElementById("dataAtual");
+
+    if (el) {
+        el.innerText = `Hoje é ${data}`;
+    }
 }
 
-iniciarSistema();
+mostrarData();
